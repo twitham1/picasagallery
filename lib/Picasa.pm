3acb046be9f6b56de46a63eee500b130d619cc3f
Timothy D Witham <twitham@sbcglobal.net>
2013-12-21 21:31:52 +0000

    First version to support browser.

--- lib/Picasa.pm
+++ lib/Picasa.pm
@@ -1,23 +1,71 @@
-# perl access to local Picasa picture database (faces, albums, etc.)
+# Picasa.pm
+
+# perl access to local Picasa picture database (faces, albums, stars, etc.)
 
 # by twitham@sbcglobal.net, 2013-06
 
+# {dirs}{<dir>}{<file>} => { field => value} is the raw .picasa.ini data only
+# {pics}{<path>} => { metadata } is all data for each picture, in perl format
+# {root}{virtual path} => physicalpath is the virtual hierarchy
+# {dir} => { current virtual "directory/" after filtering/navigating }
+# {file} => { current virtual focused "file/?" after filtering/navigating }
+# {index} => current file index ( should this be in {dir} ? )
+
+# TODO:	add filters: stars, uploads, faces, age, [ movies, geotags, captions ]
+# TODO:	Tk browser that can replace mythgallery
+
 package Picasa;
+use strict;
+use warnings;
 use File::Find;
 use File::Basename;
+use Image::ExifTool qw(:Public);
 use Data::Dumper;
 
 my $db;	    # picasa database pointer needed for File::Find's _wanted.
 
-# return new picasa database object of given directories (or empty)
+my $conf = {			# override in first arg to new
+    reject  => 'PATTERN OF FILES TO REJECT',
+    keep    => '(?i)\.jpe?g$',	# pattern of files to keep
+    datefmt => '%Y-%m-%d.%H:%M:%S', # must be sortable order
+    update => sub {},	       # callback as each directory is scanned
+    debug => 0,		       # diagnostics to STDERR
+};
+
+my $exiftool;			# exif data we are interested in:
+my $exif = [qw/AllDates FileModifyDate Caption-Abstract Description/];
+
+# return new empty picasa database object
 sub new {
     my $class = shift;
     my $self  = {};
+    bless ($self, $class);
+    if (ref $_[0]) {
+	my $hash = shift;
+	while (my($k, $v) = each %$hash) {
+	    $conf->{$k} = $v;
+	}
+    }
+    $exiftool = new Image::ExifTool;
+    $exiftool->Options(FastScan => 1,
+		       DateFormat => $conf->{datefmt});
+    $self->{dir} = $self->{file} = $self->filter('/');
+    $self->{index} = 0;
+    return $self;
+}
+
+# find pictures and picasa data in given directories
+sub recursedirs {
+    my $self = shift;
     for (@_) {
 	readdb($self, $_);
     }
-    bless ($self, $class);
-    return $self;
+    # return if $self->{file} and $self->{dir}
+    # $self->{dir} = $self->filter('/');
+    # unless ($self->{file} and $self->{file}{file}) {
+    # 	$self->{index} = -1;
+    # 	$self->next;
+    # }
 }
 
 # recursively add given directory or . to picasa database
@@ -25,7 +73,135 @@ sub readdb {
     my $self = shift;
     my $dir = shift || '.';
     $db = $self;
-    find ({ wanted => \&_wanted, no_chdir => 1 }, $dir);
+    warn "readdb $dir\n" if $conf->{debug};
+    find ({ no_chdir => 1,
+	    preprocess => sub { sort @_ },
+	    wanted => \&_wanted,
+	    postprocess => $conf->{update},
+	  }, $dir);
+}
+
+# twiddle location in the virtual tree and selected node (file)
+
+sub dirfile { # similar to fileparse, but leave trailing / on directories
+    my($path) = @_;
+    my $end = $path =~ s@/+$@@ ? '/' : '';
+    my($dir, $file) = ('/', '');
+    ($dir, $file) = ($1, $2) if $path =~ m!(.*/)([^/]+)$!;
+    return "$dir", "$file$end";
+}
+
+# TODO: option to automove to next directory if at end of this one
+sub next {
+    my($self) = @_;
+    $self->{index}++;
+    my @child = @{$self->{dir}{children}};
+    my $n = @child;
+    $self->{index} = $n - 1 if $self->{index} >= $n;
+    $self->{index} = 0 if $self->{index} < 0;
+    $self->{file} = $self->filter("$self->{dir}{dir}$self->{dir}{file}$child[$self->{index}]");
+}
+# TODO: option to automove to prev directory if at beginning of this one
+sub prev {
+    my($self) = @_;
+    $self->{index}--;
+    my @child = @{$self->{dir}{children}};
+    my $n = @child;
+    $self->{index} = 0 if $self->{index} < 0;
+    $self->{file} = $self->filter("$self->{dir}{dir}$self->{dir}{file}$child[$self->{index}]");
+}
+# back up into parent directory, with its last file selected
+sub up {
+    my($self) = @_;
+    my $file = $self->{dir}{file}; # current location should be selected after up
+    warn "chdir $self->{dir}{dir}\n" if $conf->{debug};
+    $self->{dir} = $self->filter("$self->{dir}{dir}");
+    my $index = 0;
+    for my $c (@{$self->{dir}{children}}) {
+	last if $c eq $file and $file = '!file found!';
+	$index++;
+    }
+    $index = 0 if $file ne '!file found!';
+    $self->{index} = $index - 1;
+    $self->next;
+}
+# step into {file} of current {dir}
+sub down {
+    my($self) = @_;
+    return 0 unless $self->{file}{file} =~ m!/$!;
+    warn "chdir $self->{file}{dir}$self->{file}{file}\n" if $conf->{debug};
+    $self->{dir} = $self->filter("$self->{file}{dir}$self->{file}{file}");
+    $self->{index} = -1;
+    $self->next;
+    return 1;
+}
+
+# given a virtual path, return all data known about it with current filters
+sub filter {
+    my($self, $path) = @_;
+    my $data = {};
+    my %child;			# children of this parent
+    my %face;			# faces in this path
+    my %album;			# albums in this path
+    my %done;			# files that have been processed
+    $path =~ s@/+@/@g;
+    ($data->{dir}, $data->{file}) = dirfile $path;
+    warn "filter:$path->($data->{dir},$data->{file})\n" if $conf->{debug};
+    for my $str (keys %{$self->{root}}) { # for each picture file
+	next unless 0 == index($str, $path); # match
+	next unless my $filename = $self->{root}{$str}; # physical path
+	next unless my $this = $self->{pics}{$filename}; # metadata
+# TODO: next unless matches filters
+	warn "looking at ($path) in ($str)\n" if $conf->{debug} > 1;
+	if ($str eq $path) {	# filename
+	    map { $data->{$_} = $this->{$_} } keys %$this;
+	} else { 		# directory
+	    my $rest = substr $str, length $path;
+	    $rest =~ s!/.*!/!;
+	    $rest and $child{$rest}++; # entries in this directory
+	    next if $done{$filename}++;
+	    warn "$path: $str ($rest)\n" if $conf->{debug} > 1;
+	    for my $num (qw/bytes stars uploads width height/) {
+		$data->{$num} += $this->{$num};
+	    }
+	    map { $face{$_->[0]}++ } @{$this->{faces}};
+	    map { $album{$_}++ } @{$this->{albums}};
+	    $data->{caption} += $this->{caption} ? 1 : 0;
+	}
+	$data->{pixels} += $this->{width} * $this->{height};
+	$data->{files}++;
+
+	# TODO: directories should get several samples rather than 1:
+	$data->{physical} or $data->{physical} = $filename;
+
+	$data->{time} = $this->{time} unless 
+	    $data->{time} && $data->{time} lt $this->{time};
+	$data->{endtime} = $this->{time} unless 
+	    $data->{endtime} && $data->{endtime} gt $this->{time};
+    }
+    $data->{children} = [sort keys %child]; # maybe sort later? sort by option?
+    $data->{faces} or $data->{faces} = [keys %face];
+    $data->{albums} or $data->{albums} = [keys %album];
+    warn "filtered $path: ", Dumper $data if $conf->{debug} > 2;
+    return $data;
+}
+
+# add picture to virtual path
+sub _addpic2path {
+    my($self, $path, $key) = @_;
+    $self->{root}{$path} = $key;
+}
+
+# IDEA: given a picture, return all info about it in easy to use form
+sub picdata {
+    my($self, $dir, $pic) = @_;
+    my $out = {};
+    my $file = "$dir$pic";
+    return $out unless -s $file;
+    if (my @faces = $self->faces($dir, $pic)) {
+	warn "add face to $dir$pic @faces\n" if $conf->{debug} > 1;
+	$out->{faces} = [ @faces ];
+    }
 }
 
 # return all directories of the database
@@ -38,8 +214,7 @@ sub dirs {
 sub pics {
     my $self = shift;
     my $dir = shift or return ();
-    my $pat = shift || '(?i).jpg$';
-    return grep /$pat/, keys %{$self->{dirs}{$dir}};
+    return grep !/^<<\w+>>$/, keys %{$self->{dirs}{$dir}};
 }
 
 # return all contacts of the database
@@ -67,7 +242,7 @@ sub picasa {
     return $self->{dirs}{$dir}{$pic} || {};
 }
 
-# return array of [id, name, NW, SE] of faces in given $dir $pic
+# return array of [id, NW, SE] of faces in given $dir $pic
 sub faces {
     my($self, $dir, $pic) = @_;
     my @ret;
@@ -76,29 +251,33 @@ sub faces {
     return @ret unless $this and $this->{faces};
     for my $string (split ';', $this->{faces}) {
 	my($rect, $id) = split ',', $string;
-	my $name = $self->contact2person($id);
-	push @ret, [$id, $name, $self->rect($rect)];
+	push @ret, [$id, $self->rect($rect)];
     }
     return @ret;
 }
 
-# return array of [id, name, date, location, description] of albums in $pic
+# return array of album ids in $pic
 sub albums {
     my($self, $dir, $pic) = @_;
-    my @ret;
-    return @ret unless $dir and $pic;
+    return () unless $dir and $pic;
     my $this = $self->{dirs}{$dir}{$pic};
-    return @ret unless $this and $this->{albums};
-    for my $id (split ',', $this->{albums}) {
-	my $this = $self->{album}{$id};
-	push @ret, [$id,
-		    $this->{name} || '',
-		    $this->{date} || '',
-		    $this->{location} || '',
-		    $this->{description} || '',
-	];
-    }
-    return @ret;
+    return () unless $this and $this->{albums};
+    return split ',', $this->{albums};
+}
+
+sub star {
+    my($self, $dir, $pic) = @_;
+    return 0 unless $dir and $pic;
+    my $this = $self->{dirs}{$dir}{$pic};
+    return 0 unless $this and $this->{star};
+    return 1;
+}
+
+sub uploads {
+    my($self, $dir, $pic) = @_;
+    return 0 unless $dir and $pic;
+    my $this = $self->{dirs}{$dir}{$pic};
+    return scalar grep /^IIDLIST_/, keys %$this;
 }
 
 # write the .picasa.ini in given $dir, backing up the original once
@@ -112,6 +291,8 @@ sub save {
     }
     my $now;
     for my $file (sort keys %{$self->{dirs}{$dir}}) {
+	next if $file =~ m@/$@;	# skip subdirs
+	next if $file =~ m@^<<\w+>>$@; # skip internal metadata
 	if (my @key = sort keys %{$self->{dirs}{$dir}{$file}}) {
 	    $now .= ($file =~ /\[.+\]/ ? $file : "[$file]") . "\r\n";
 	    for my $f (@key) {
@@ -174,7 +355,7 @@ sub _merge {
 	    } elsif ($b->{$_} and ! $a->{$_}) {
 		$c->{$_} = $b->{$_};
 	    } else {
-		warn "WARN $_: (keep:) $a->{$_} (lose:) $b->{$_}\n";
+		warn "WARN $_: (keep:) $a->{$_} (lose:) $b->{$_}\n" if $conf->{debug};
 		$c->{$_} = $a->{$_};
 	    }
 	} elsif (defined $a->{$_}) {
@@ -192,13 +373,66 @@ sub _merge {
 # add a file or directory to the database
 sub _wanted {
     my($file, $dir) = fileparse $_;
+#    $dir = '' if $dir eq '.';
     if ($file eq '.picasa.ini' or $file eq 'Picasa.ini') {
 	&_understand($db, _readfile($_));
-    } elsif ($file =~ /^\..+/ or $file eq 'Originals') { # ignore hidden files/directories
+	return;
+    } elsif ($file =~ /^\..+/ or $file eq 'Originals') { # ignore hidden files
 	$File::Find::prune = 1;
-    } elsif (-f $_) {
+	return;
+    }
+    return if $file =~ /$conf->{reject}/;
+    if (-f $_) {
+	return unless $file =~ /$conf->{keep}/;
 	$db->{dirs}{$dir}{$file} = {}
 	unless $db->{dirs}{$dir}{$file};
+	my $key = $_;
+	$key =~ s@\./@@;
+	return unless -f $key and -s $key > 100;
+	my $info = $exiftool->ImageInfo($key); #, $exif);
+#	my $info = $exiftool->ImageInfo($key, $exif);
+	return unless $info;
+	my $this = $db->{pics}{$key} = {
+	    bytes	=> -s $key,
+	    width	=> $info->{ImageWidth} || 0,
+	    height	=> $info->{ImageHeight} || 0,
+	    time	=> $info->{DateTimeOriginal} || $info->{CreateDate}
+	    || $info->{ModifyDate} || $info->{FileModifyDate} || 0,
+	    caption	=> $info->{'Caption-Abstract'}
+	    || $info->{'Description'} || '',
+	    faces	=> [ $db->faces($dir, $file) ],
+	    albums	=> [ $db->albums($dir, $file) ],
+	    stars	=> $db->star($dir, $file),
+	    uploads	=> $db->uploads($dir, $file),
+	};
+	$this->{time} =~ /0000/ and
+	    warn "bogus time in $_: $this->{time}\n";
+	my $year = 0;
+	$year = $1 if $this->{time} =~ /(\d{4})/; # year must be first
+
+	my $vname = "$this->{time}-$file"; # unique virtual filename
+
+	# add albums (starred, named)
+	$this->{stars} and
+	    $db->_addpic2path("/[Albums]/Starred Photos/$year/$vname", $key);
+	for my $id (@{$this->{albums}}) {
+	    next unless my $name = $db->{album}{$id}{name};
+	    $db->_addpic2path("/[Albums]/$name/$vname", $key);
+	}
+
+	# add faces / people
+	for my $face ($db->faces($dir, $file)) {
+	    my($id) = @$face;
+	    next unless my $name = $db->contact2person($id);
+	    $db->_addpic2path("/[People]/$name/$year/$vname", $key);
+	}
+
+	# add folders
+	$db->_addpic2path("/[Folders]/$key", $key);
+
+    } elsif (-d $_) {
+	$db->{dirs}{$dir}{"$file/"} = {}
+	unless $db->{dirs}{$dir}{"$file/"};
     }
 }
 
@@ -207,7 +441,6 @@ sub _readfile {
     my($file) = @_;
     my $data = {};
     my $fh;
-#    warn ">$file<\n";
     return $data unless open $fh, $file;
     my $section = '';
     my($name, $dir) = fileparse $file;
@@ -215,7 +448,7 @@ sub _readfile {
     while (<$fh>) {
 	chomp;
 	s/\r*\n*$//;
-	s/\&\#(\d{3});/sprintf "%c", oct($1)/eg;
+	s/\&\#(\d{3});/sprintf "%c", oct($1)/eg; # will this corrupt if we write it out later?
 	if (/^\[([^\]]+)\]/) {
 	    $section = $1;
 	} elsif (my($k, $v) = split '=', $_, 2) {
@@ -252,7 +485,9 @@ sub _understand {
 sub _identical {
     my($a, $b) = @_;
     for my $k (keys %$a, keys %$b) {
-	$a->{$k} eq $b->{$k} or warn "$k: (keep:) $a->{$k} (lose:) $b->{$k} ($File::Find::name)\n";
+	$a->{$k} eq $b->{$k} or
+	    $conf->{debug} and
+	    warn "$k: (keep:) $a->{$k} (lose:) $b->{$k} ($File::Find::name)\n";
     }
     return $a;
 }
